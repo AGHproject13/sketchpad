6c4fb2ceae56e76a20b4e13f6cda85b235d4fba2
AGHproject13 <agehaproject13@gmail.com>
2023-03-11 13:30:50 +0700

    Add button to change the grid size
    Create a new function makeSketchpad so that changeGridSize function (added to the button) can make a new sketchpad according to the user's desired size.

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const container = document.querySelector(".container");
-const gridRatio = 48;
-for (let i = 0; i < gridRatio; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < gridRatio; j++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    column.addEventListener("mouseover", addColor);
-    row.appendChild(column);
+let container = document.querySelector(".container");
+const button = document.querySelector("header button");
+button.addEventListener("click", changeGridSize);
+// Default grids per side = 16
+makeSketchpad(16);
+
+function makeSketchpad(gridRatio) {
+  // Make a sketchpad with x number of grids per side with two-dimensional loop
+  for (let i = 0; i < gridRatio; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < gridRatio; j++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      column.addEventListener("mouseover", addColor);
+      row.appendChild(column);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
+}
+
+function changeGridSize() {
+  const userPrompt = +prompt("Please enter grid size: ");
+  if (!Number.isInteger(userPrompt) || userPrompt > 100) return;
+  // Remove the old container and make a new one with the correct size
+  document.body.removeChild(container);
+  const newSketchpad = document.createElement("div");
+  newSketchpad.classList.add("container");
+  // Replace the container with the new one and append it to body
+  container = newSketchpad;
+  document.body.appendChild(container);
+  makeSketchpad(userPrompt);
 }
 
 function addColor() {
